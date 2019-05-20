39982aba82fd6688ab0358bbdeb04c5e162d46fc
Nick Holvast <nick.holvast@engineering.digital.dwp.gov.uk>
2019-05-20 08:21:30 +0100

    moved constants to uglified json

--- src/index.js
+++ src/index.js
@@ -1,24 +1,21 @@
-const players = [
-  'Mo Salah','Deandre Yedlin','Joselu','James Milner','Pedro','Trent Alexander Arnold'
-]
-const clubs = [
-  'Brighton','Hull','Colo Colo','Liepzig','Napoli','South Shields'
-]
-const reasons = [
-  'wants a new challenge','is tempted by the nightlife','has fallen out with the manager','had a run in with the fans'
-]
+const players = require('./players.json')
+const clubs = require('./clubs.json')
+const reasons = require('./reasons.json')
 
-const getArrayItem = arr => arr[Math.floor(Math.random()*arr.length)]
+const getArrayItem = arr => arr[Math.floor(Math.random() * arr.length)]
 
 exports.handler = (_, __, callback) => {
   const result = `${getArrayItem(players)} has been rumoured to be on his way to ${getArrayItem(clubs)}. A source was quoted saying he ${getArrayItem(reasons)}.`
   const response = {
-    "statusCode": 200,
-    "headers": {
-      "my_header": "my_value"
+    'statusCode': 200,
+    'headers': {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+      'Access-Control-Allow-Methods': 'GET',
+      'Access-Control-Allow-Headers': 'application/json'
     },
-    "body": JSON.stringify(result),
-    "isBase64Encoded": false
+    'body': JSON.stringify(result),
+    'isBase64Encoded': false
   }
   return callback(null, response)
 }
